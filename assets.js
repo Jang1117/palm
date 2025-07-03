59b7b4146476cb753be2aed91e25066c51ba69e0
Jang1117 <7911171g@gmail.com>
2025-07-03 09:03:01 +0900

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,102 @@
+const assets = [
+    {
+        position: { lat: -2.062159, lng: 101.005916 },
+        title: "Transco Energi Utama",
+        description: "Transco Energi Utama는 인도네시아 서부 수마트라 지역에 위치한 팜 오일 생산 시설로, INCASI RAYA 그룹의 자회사입니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: -1.393349, lng: 101.593242 },
+        title: "BINA PRATAMA SAKATOJAYA",
+        description: "BINA PRATAMA SAKATOJAYA는 INCASI RAYA 그룹 소속의 팜 오일 생산 시설로, 서부 수마트라 지역에서 운영됩니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: -0.997233, lng: 101.7423 },
+        title: "SUMBAR ANDALAS KENCANA",
+        description: "SUMBAR ANDALAS KENCANA는 서부 수마트라의 Dharmasraya 지역에 위치한 INCASI RAYA 소속 팜 오일 생산 시설입니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: -2.184183, lng: 100.917317 },
+        title: "SODETAN",
+        description: "SODETAN은 INCASI RAYA 그룹의 팜 오일 생산 시설로, 서부 수마트라 Pesisir Selatan 지역에서 운영됩니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: -2.330883, lng: 100.995267 },
+        title: "SILAUT",
+        description: "SILAUT은 SUMATERA JAYA AGRO LESTARI 소속의 팜 오일 생산 시설로, INCASI RAYA 그룹의 일원입니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: -1.250588889, lng: 101.5751028 },
+        title: "SELAGO MAKMUR PLANTATION",
+        description: "SELAGO MAKMUR PLANTATION은 서부 수마트라 Dharmasraya 지역의 팜 오일 플랜테이션으로, INCASI RAYA 소속입니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: 0.362283, lng: 99.6336 },
+        title: "PASAMANAN MARAHMA SEJAHTERA",
+        description: "PASAMANAN MARAHMA SEJAHTERA는 서부 수마트라 Pasaman Barat 지역의 팜 오일 생산 시설로, INCASI RAYA 소속입니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: -1.320133, lng: 101.648567 },
+        title: "PANGIAN",
+        description: "PANGIAN은 INCASI RAYA 그룹의 팜 오일 생산 시설로, 서부 수마트라 Dharmasraya 지역에서 운영됩니다.",
+        logoUrl: "./groups/incasi-raya-logo.png",
+        imageUrl: "./images/pangian.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: 0.332883, lng: 99.3828 },
+        title: "BINTARA TANI NUSANTARA",
+        description: "BINTARA TANI NUSANTARA는 서부 수마트라 Pasaman Barat 지역의 팜 오일 생산 시설로, INCASI RAYA 소속입니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    },
+    {
+        position: { lat: 1.078939, lng: 100.055088 },
+        title: "MULTIAGRO SUMATERAJAYA",
+        description: "MULTIAGRO SUMATERAJAYA는 북부 수마트라 Padang Lawas 지역의 팜 오일 생산 시설로, INCASI RAYA 소속입니다.",
+        logoUrl: "./images/default_logo.png",
+        imageUrl: "./images/default_image.png",
+        country: "인도네시아",
+        establishedDate: "정보 없음",
+        category: "자원/에너지인프라"
+    }
+];
