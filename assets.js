a31a69ded1cfe4063e75233ad0990ba7d1c046a7
Jang1117 <7911171g@gmail.com>
2025-07-03 11:41:08 +0900

    Update assets.js

--- assets.js
+++ assets.js
@@ -1,35 +1,34 @@
 const assets = [
     {
-        position: { lat: -2.184, lng: 100.917 }, // 더미 좌표
+        position: { lat: -2.184, lng: 100.917 },
         title: "Sodetan",
         millCompanyName: "INCASI RAYA",
         parentCompanyName: "INCASI RAYA",
         address: "Ds.Muara Sakai, Kec.Pancung Soal, Pesisir Selatan, West Sumatra",
         province: "SUMATERA BARAT",
         farmArea: "12,433 ha",
-        FFBProduction: "243,148 ton/year",
+        ffbProduction: "243,148 ton/year",
         facilityCapacity: "120 ton/hour",
         cpoProduction: "46,626 ton/year",
         pkProduction: "10,341 ton/year",
-        description: "SODETAN은 INCASI RAYA 그룹의 팜 오일 생산 시설로, 서부 수마트라 Pesisir Selatan 지역에서 운영됩니다. 농장면적: 6000 ha, FFB 생산: 30000 ton/year, 설비 용량: 40 ton/hour, CPO 생산: 15000 ton/year, PK 생산: 4000 ton/year."
+        description: "농장면적: 12,433 ha, FFB 생산: 243,148 ton/year, 설비 용량: 120 ton/hour, CPO 생산: 46,626 ton/year, PK 생산: 10,341 ton/year."
     },
     {
-        position: { lat: -2.331, lng: 100.995 }, // 더미 좌표
+        position: { lat: -2.331, lng: 100.995 },
         title: "Silaut",
         millCompanyName: "SUMATERA JAYA AGRO LESTARI",
         parentCompanyName: "INCASI RAYA",
         address: "Ds.Silaut, Kec.Lunang Silaut, Pesisir Selatan, West Sumatra",
         province: "SUMATERA BARAT",
-        farmArea: "10409 ha",
+        farmArea: "10,409 ha",
         ffbProduction: "218,486 ton/year",
         facilityCapacity: "120 ton/hour",
         cpoProduction: "44,846 ton/year",
         pkProduction: "8,925 ton/year",
-        description: "SILAUT은 SUMATERA JAYA AGRO LESTARI 소속의 팜 오일 생산 시설로, INCASI RAYA 그룹의 일원입니다. 농장면적: 7000 ha, FFB 생산: 35000 ton/year, 설비 용량: 50 ton/hour, CPO 생산: 17000 ton/year, PK 생산: 4200 ton/year."
+        description: "농장면적: 10,409 ha, FFB 생산: 218,486 ton/year, 설비 용량: 120 ton/hour, CPO 생산: 44,846 ton/year, PK 생산: 8,925 ton/year."
     },
-
     {
-        position: { lat: -1.393, lng: 101.593 }, // 더미 좌표
+        position: { lat: -1.393, lng: 101.593 },
         title: "Bina Pratama Sakato Jaya",
         millCompanyName: "BINA PRATAMA SAKATOJAYA",
         parentCompanyName: "INCASI RAYA",
@@ -40,10 +39,10 @@ const assets = [
         facilityCapacity: "60 ton/hour",
         cpoProduction: "12,079 ton/year",
         pkProduction: "2,986 ton/year",
-        description: "BINA PRATAMA SAKATOJAYA는 INCASI RAYA 그룹 소속의 팜 오일 생산 시설로, 서부 수마트라 지역에서 운영됩니다. 농장면적: 7500 ha, FFB 생산: 38000 ton/year, 설비 용량: 50 ton/hour, CPO 생산: 18000 ton/year, PK 생산: 4500 ton/year."
+        description: "농장면적: 4,095 ha, FFB 생산: 64,038 ton/year, 설비 용량: 60 ton/hour, CPO 생산: 12,079 ton/year, PK 생산: 2,986 ton/year."
     },
     {
-        position: { lat: -0.997, lng: 101.742 }, // 더미 좌표
+        position: { lat: -0.997, lng: 101.742 },
         title: "Sumbar Andalas Kencana",
         millCompanyName: "SUMBAR ANDALAS KENCANA",
         parentCompanyName: "INCASI RAYA",
@@ -54,12 +53,10 @@ const assets = [
         facilityCapacity: "60 ton/hour",
         cpoProduction: "15,105 ton/year",
         pkProduction: "2,784 ton/year",
-        description: "SUMBAR ANDALAS KENCANA는 서부 수마트라의 Dharmasraya 지역에 위치한 INCASI RAYA 소속 팜 오일 생산 시설입니다. 농장면적: 9000 ha, FFB 생산: 45000 ton/year, 설비 용량: 70 ton/hour, CPO 생산: 22000 ton/year, PK 생산: 5500 ton/year."
+        description: "농장면적: 6,344 ha, FFB 생산: 78,224 ton/year, 설비 용량: 60 ton/hour, CPO 생산: 15,105 ton/year, PK 생산: 2,784 ton/year."
     },
-
-
     {
-        position: { lat: -1.251, lng: 101.575 }, // 더미 좌표
+        position: { lat: -1.251, lng: 101.575 },
         title: "Selago Makmur Plantation",
         millCompanyName: "SELAGO MAKMUR PLANTATION",
         parentCompanyName: "INCASI RAYA",
@@ -70,10 +67,10 @@ const assets = [
         facilityCapacity: "80 ton/hour",
         cpoProduction: "11,701 ton/year",
         pkProduction: "3,140 ton/year",
-        description: "SELAGO MAKMUR PLANTATION은 서부 수마트라 Dharmasraya 지역의 팜 오일 플랜테이션으로, INCASI RAYA 소속입니다. 농장면적: 8000 ha, FFB 생산: 40000 ton/year, 설비 용량: 60 ton/hour, CPO 생산: 19000 ton/year, PK 생산: 4800 ton/year."
+        description: "농장면적: 6,065 ha, FFB 생산: 58,829 ton/year, 설비 용량: 80 ton/hour, CPO 생산: 11,701 ton/year, PK 생산: 3,140 ton/year."
     },
     {
-        position: { lat: 0.362, lng: 99.634 }, // 더미 좌표
+        position: { lat: 0.362, lng: 99.634 },
         title: "Pasamanan Marahma Sejahtera",
         millCompanyName: "PASAMANAN MARAHMA SEJAHTERA",
         parentCompanyName: "INCASI RAYA",
@@ -84,10 +81,10 @@ const assets = [
         facilityCapacity: "40 ton/hour",
         cpoProduction: "2,145 ton/year",
         pkProduction: "619 ton/year",
-        description: "PASAMANAN MARAHMA SEJAHTERA는 서부 수마트라 Pasaman Barat 지역의 팜 오일 생산 시설로, INCASI RAYA 소속입니다. 농장면적: 6500 ha, FFB 생산: 32000 ton/year, 설비 용량: 45 ton/hour, CPO 생산: 16000 ton/year, PK 생산: 4100 ton/year."
+        description: "농장면적: 4,025 ha, FFB 생산: 11,209 ton/year, 설비 용량: 40 ton/hour, CPO 생산: 2,145 ton/year, PK 생산: 619 ton/year."
     },
     {
-        position: { lat: 0.333, lng: 99.383 }, // 더미 좌표
+        position: { lat: 0.333, lng: 99.383 },
         title: "Bintara Tani Nusantara",
         millCompanyName: "BINTARA TANI NUSANTARA",
         parentCompanyName: "INCASI RAYA",
@@ -98,10 +95,10 @@ const assets = [
         facilityCapacity: "100 ton/hour",
         cpoProduction: "37,769 ton/year",
         pkProduction: "8,315 ton/year",
-        description: "BINTARA TANI NUSANTARA는 서부 수마트라 Pasaman Barat 지역의 팜 오일 생산 시설로, INCASI RAYA 소속입니다. 농장면적: 6000 ha, FFB 생산: 30000 ton/year, 설비 용량: 40 ton/hour, CPO 생산: 15000 ton/year, PK 생산: 4000 ton/year."
+        description: "농장면적: 7,185 ha, FFB 생산: 172,870 ton/year, 설비 용량: 100 ton/hour, CPO 생산: 37,769 ton/year, PK 생산: 8,315 ton/year."
     },
     {
-        position: { lat: -1.320, lng: 101.649 }, // 더미 좌표
+        position: { lat: -1.320, lng: 101.649 },
         title: "Pangian",
         millCompanyName: "INCASI RAYA",
         parentCompanyName: "INCASI RAYA",
@@ -112,6 +109,6 @@ const assets = [
         facilityCapacity: "50 ton/hour",
         cpoProduction: "(no data) ton/year",
         pkProduction: "(no data) ton/year",
-        description: "PANGIAN은 INCASI RAYA 그룹의 팜 오일 생산 시설로, 서부 수마트라 Dharmasraya 지역에서 운영됩니다. 농장면적: 7000 ha, FFB 생산: 34000 ton/year, 설비 용량: 50 ton/hour, CPO 생산: 17000 ton/year, PK 생산: 4300 ton/year."
+        description: "농장면적: 6,899 ha, FFB 생산: (no data) ton/year, 설비 용량: 50 ton/hour, CPO 생산: (no data) ton/year, PK 생산: (no data) ton/year."
     }
 ];
